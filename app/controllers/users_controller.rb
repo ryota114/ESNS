efcc53569335133d5ba0cb574b7e7b0bd0f7b79e
ryota114 <namahann0978@gmail.com>
2021-06-02 07:57:20 +0000

    [Update]users/my_page
    アカウント退会処理の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,43 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def show
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def index
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to my_page_users_path
+  end
+
+  def follow
+  end
+
+  def follower
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+    @user = User.find(current_user.id)
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:introduction,:image)
+  end
+
 end
