4e2138bc5cc81134f9f00e9929b1eef6d9b914aa
ryota114 <namahann0978@gmail.com>
2021-06-06 11:56:37 +0000

    [Add]posts/edit
    投稿の編集機能wo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,9 +23,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   def destroy
